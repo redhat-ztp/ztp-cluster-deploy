c1c799990496e79fbe1a6ffb804e2c3c2ebba62b
melserngawy <melserng@redhat.com>
2021-01-29 17:10:42 -0500

    amend
    Signed-off-by: melserngawy <melserng@redhat.com>

--- acm-spoke/import_cluster.sh
+++ acm-spoke/import_cluster.sh
@@ -1,28 +1,36 @@
 #!/usr/bin/env bash
 
 CLUSTER_NAME=$1
-KUBE_CONFIG=$2
+PROFILE=$2
+KUBE_CONFIG=$3
 
-cat << EOF >> ./import-$CLUSTER_NAME.yaml
+
+cat << EOF > ./$CLUSTER_NAME.yaml
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $CLUSTER_NAME-cluster
+---
 apiVersion: cluster.open-cluster-management.io/v1
 kind: ManagedCluster
 metadata:
   labels:
     cloud: auto-detect
     vendor: auto-detect
-    name: $CLUSTER_NAME
-  name: $CLUSTER_NAME
+    name: $CLUSTER_NAME-cluster
+    profile: $PROFILE
+  name: $CLUSTER_NAME-cluster
 spec:
   hubAcceptsClient: true
 ---
 apiVersion: agent.open-cluster-management.io/v1
 kind: KlusterletAddonConfig
 metadata:
-  name: $CLUSTER_NAME
-  namespace: $CLUSTER_NAME
+  name: $CLUSTER_NAME-cluster
+  namespace: $CLUSTER_NAME-cluster
 spec:
-  clusterName: $CLUSTER_NAME
-  clusterNamespace: $CLUSTER_NAME
+  clusterName: $CLUSTER_NAME-cluster
+  clusterNamespace: $CLUSTER_NAME-cluster
   clusterLabels:
     cloud: auto-detect
     vendor: auto-detect
